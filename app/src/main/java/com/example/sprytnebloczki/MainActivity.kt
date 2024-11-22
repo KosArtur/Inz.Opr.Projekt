de25eee9b6d40aef1ba8ccd9bdc158ed08835164
Ewa Szymik <ewaszymik25@gmail.com>
2024-11-22 20:19:18 +0100

    adding blocs to workspace

--- app/src/main/java/com/example/sprytnebloczki/MainActivity.kt
+++ app/src/main/java/com/example/sprytnebloczki/MainActivity.kt
@@ -1,25 +1,35 @@
 package com.example.sprytnebloczki
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Button
+import android.view.Gravity
+import android.view.View
+import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.PopupMenu
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var buttonRun : TextView;
-    private lateinit var buttonOptions : TextView;
-    private lateinit var buttonCode : TextView;
 
+    private lateinit var buttonRun : TextView
+    private lateinit var buttonOptions : TextView
+    private lateinit var buttonCode : TextView
+    private lateinit var startBloc : ImageView
+    private lateinit var endBloc : ImageView
+    private lateinit var operationBloc : ImageView
+    private lateinit var ifBloc : ImageView
+    private lateinit var inputBloc : ImageView
+    private lateinit var instruction: TextView
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,11 +40,39 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+
+
+
+        startBloc=findViewById(R.id.start)
+        endBloc=findViewById(R.id.koniec)
+        operationBloc=findViewById(R.id.operacja)
+        ifBloc=findViewById(R.id.warunek)
+        inputBloc=findViewById(R.id.io)
+        instruction=findViewById(R.id.instruction)
+
+        startBloc.setOnClickListener{
+           addBloc("start")
+            instruction.visibility= View.GONE
+        }
+        endBloc.setOnClickListener{
+            addBloc("koniec")
+        }
+        operationBloc.setOnClickListener{
+            addBloc("operacja")
+        }
+        ifBloc.setOnClickListener{
+            addBloc("warunek")
+        }
+        inputBloc.setOnClickListener{
+            addBloc("input")
+        }
+
+
         buttonRun=findViewById(R.id.buttonRun)
         buttonOptions=findViewById(R.id.buttonOptions)
         buttonCode=findViewById(R.id.buttonCode)
 
-        buttonOptions.setOnClickListener(){
+        buttonOptions.setOnClickListener{
 
             val popupMenu = PopupMenu(this, buttonOptions, 0,0, R.style.CustomPopupMenu)
             popupMenu.menuInflater.inflate(R.menu.toolbar, popupMenu.menu)
@@ -71,7 +109,59 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    @SuppressLint("ClickableViewAccessibility")
+    fun addBloc(type: String ){
+        val rootLayout = findViewById<FrameLayout>(R.id.root)
+        val icon = ImageView(this).apply {
+
+            val imageResource = when (type) {
+                "koniec" -> R.drawable.koniec
+                "operacja" -> R.drawable.operacja
+                "input" -> R.drawable.input
+                "warunek" -> R.drawable.warunek
+                else -> R.drawable.start
+            }
+            setImageResource(imageResource)
+            if(type=="start" || type=="koniec"){
+                layoutParams = FrameLayout.LayoutParams(300, 300).apply {
+
+                    leftMargin = 100
+                    topMargin = 100
+                }
+            } else{
+                layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT).apply {
+
+                    gravity=Gravity.CENTER
+                }
+            }
+        }
+
+        if(type=="start" || type=="koniec"){
+            rootLayout.addView(icon)
+            icon.setOnTouchListener(DraggableItem())
+        } else{
+
+            val editText = EditText(this).apply {
+                hint = "Type..."
+                layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT).apply {
+                    gravity = Gravity.CENTER
+                }
+                textSize = 16f
+            }
 
 
+            val iconWithText = FrameLayout(this).apply {
+                layoutParams = FrameLayout.LayoutParams(360 , if(type == "warunek") 360 else 140).apply {
+                    leftMargin =400
+                    topMargin = 400
+                }
 
+            }
+            iconWithText.addView(icon)
+            iconWithText.addView(editText)
+            rootLayout.addView(iconWithText)
+            iconWithText.setOnTouchListener(DraggableItem())
+        }
+    }
 }
+
